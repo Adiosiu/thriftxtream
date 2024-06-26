9cd448cdd4c8abc7180fe43d24ec01e083b50882
Adiosiu <komangadi369@gmail.com>
2024-06-26 14:14:02 +0800

    Create Theme.js

---
+++ Theme.js
@@ -0,0 +1,14 @@
+// theme.js
+import { extendTheme } from '@chakra-ui/react';
+
+const theme = extendTheme({
+  styles: {
+    global: {
+      body: {
+        fontFamily: 'Arial, sans-serif',
+      },
+    },
+  },
+});
+
+export default theme;
